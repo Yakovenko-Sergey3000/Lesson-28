d812ecd9745237005d5a7f4eb7f98af31c8eb01c
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-04-25 18:41:20 +0500

    add post link

--- app.rb
+++ app.rb
@@ -48,7 +48,6 @@ post '/new' do
       @db.execute 'insert into Posts (content, created_date) values (?, datetime());', [@content]
 
       #перенаправление на главную страницу 
-      redirect to '/'
-    
+      redirect to '/'   
 end  
 
