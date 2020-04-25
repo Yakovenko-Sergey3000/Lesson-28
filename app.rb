3bb006ecb2789f2939662884c2fd5464d83ed014
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-04-25 17:54:34 +0500

    Add @error

--- app.rb
+++ app.rb
@@ -10,11 +10,15 @@ def init_db
 end
 
 before do 
+  #инициализация БД
   init_db
 end
 
+# configure - Вызывается каждый раз при конфигурации приожения:
+# когда изменяется код прграммы и перезрузилась страница
 configure do 
   init_db
+  #создает таблицу если она не существует, а если она есть то ничего не происходит 
  @db.execute  'CREATE TABLE if not exists Posts 
   (
     id integer PRIMARY KEY AUTOINCREMENT,
@@ -34,6 +38,10 @@ end
 post '/new' do 
     @content = params[:content]
 
+      if @content.size <= 0 
+         @error = "Type post text "
+         return erb :new 
+      end  
     erb "You typed #{@content}"
 end  
 
